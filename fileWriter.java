77ca1e282850506f0f451996f4c12f250bdbaae6
danyul <danieltonyh06@gmail.com>
2023-08-25 22:38:03 -0700

    FileWriter complete
    adding file writer with all the proper functionality along with a blank file reader class

---
+++ fileWriter.java
@@ -0,0 +1,22 @@
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+public class fileWriter {
+    public static void main(String[] args) throws FileNotFoundException {
+        writeText(generateRandomString());
+    }
+
+    public static void writeText(String file) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(file);
+        pw.print("Printing this on given file.");
+        pw.close();
+    }
+
+    public static String generateRandomString() {
+        String fileName = "";
+        for (int i = 0; i < 5; i++) {
+            fileName += (((int) (Math.random() * 9) + 1) + ((char) ((int) (Math.random() * 9) + 1)));
+        }
+        return fileName;
+    }
+}
